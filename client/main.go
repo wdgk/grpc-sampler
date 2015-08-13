67b11b022aeee05f6d41f49f07ecf04b4e2f502e
gaku <kuga729@gmail.com>
2015-08-13 15:12:02 +0900

    modify client and server

---
+++ client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	pb "github.com/wdgk/grpc-sampler/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+var (
+	addrFlag = flag.String("addr", "localhost:5000", "server address host:post")
+)
+
+func main() {
+	conn, err := grpc.Dial(*addrFlag)
+
+	if err != nil {
+		log.Fatalf("Connection error: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	resp, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: "Wada"})
+	if err != nil {
+		log.Fatalf("RPC error: %v", err)
+	}
+	log.Printf("Greeting: %s", resp.Message)
+}
--- server/main.go
+++ server/main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"flag"
+	pb "github.com/wdgk/grpc-sampler/proto"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	"log"
 	"net"
-	pb "proto/helloworld.proto"
+)
+
+var (
+	addrFlag = flag.String("addr", ":5000", "Address host:post")
 )
 
 type server struct{}
 
-func (s *server) SayHello(contrxt context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("New Request: %v", in.String())
+	return &pb.HelloReply{Message: "Hello, " + in.Name + "!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", *addrFlag)
+
+	if err != nil {
+		log.Fatalf("boo")
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
 
 }
